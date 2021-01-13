c849f5602f6771a435e81c88da478e6794e53683
DESKTOP-0N31KU4\j0dev <rig0408@naver.com>
2021-01-13 19:02:50 +0900

    Site updated: 2021-01-13 19:02:49

--- js/main.js
+++ js/main.js
@@ -1,192 +1,239 @@
-/* eslint-disable node/no-unsupported-features/node-builtins */
-(function($, moment, ClipboardJS, config) {
-    $('.article img:not(".not-gallery-item")').each(function() {
-        // wrap images with link and add caption if possible
-        if ($(this).parent('a').length === 0) {
-            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
-            if (this.alt) {
-                $(this).after('<p class="has-text-centered is-size-6 caption">' + this.alt + '</p>');
+/* eslint-disable no-undef */
+// console.js
+'use strict'
+console.log(String.raw`
+          _____                    _____                    _____                    _____          
+         /\    \                  /\    \                  /\    \                  /\    \         
+        /::\____\                /::\    \                /::\    \                /::\    \        
+       /:::/    /               /::::\    \               \:::\    \              /::::\    \       
+      /:::/    /               /::::::\    \               \:::\    \            /::::::\    \      
+     /:::/    /               /:::/\:::\    \               \:::\    \          /:::/\:::\    \     
+    /:::/____/               /:::/__\:::\    \               \:::\    \        /:::/__\:::\    \    
+   /::::\    \              /::::\   \:::\    \               \:::\    \      /::::\   \:::\    \   
+  /::::::\____\________    /::::::\   \:::\    \               \:::\    \    /::::::\   \:::\    \  
+ /:::/\:::::::::::\    \  /:::/\:::\   \:::\    \               \:::\    \  /:::/\:::\   \:::\    \ 
+/:::/  |:::::::::::\____\/:::/  \:::\   \:::\____\_______________\:::\____\/:::/__\:::\   \:::\____\
+\::/   |::|~~~|~~~~~     \::/    \:::\  /:::/    /\::::::::::::::::::/    /\:::\   \:::\   \::/    /
+ \/____|::|   |           \/____/ \:::\/:::/    /  \::::::::::::::::/____/  \:::\   \:::\   \/____/ 
+       |::|   |                    \::::::/    /    \:::\~~~~\~~~~~~         \:::\   \:::\    \     
+       |::|   |                     \::::/    /      \:::\    \               \:::\   \:::\____\    
+       |::|   |                     /:::/    /        \:::\    \               \:::\   \::/    /    
+       |::|   |                    /:::/    /          \:::\    \               \:::\   \/____/     
+       |::|   |                   /:::/    /            \:::\    \               \:::\    \         
+       \::|   |                  /:::/    /              \:::\____\               \:::\____\        
+        \:|   |                  \::/    /                \::/    /                \::/    /        
+         \|___|                   \/____/                  \/____/                  \/____/         
+see theme at https://github.com/theme-kaze/hexo-theme-kaze
+`)
+// darkmode.js
+// reverse button
+const scrollWidth =
+  document.body.scrollWidth || document.documentElement.scrollWidth
+let darkControlButton = null
+let searchControlButton = null
+if (scrollWidth <= 742) {
+  darkControlButton = document.getElementsByClassName('darkwidget')[0]
+  searchControlButton = document.getElementsByClassName('searchwidget')[0]
+} else {
+  darkControlButton = document.getElementsByClassName('darknavbar')[0]
+  searchControlButton = document.getElementsByClassName('searchnavbar')[0]
+}
+
+darkControlButton.addEventListener('click', () => {
+  setDarkmode(reverseDarkModeSetting())
+})
+// scroll-up.js
+const smoothScrollToTop = () => {
+  let yTopValve =
+    window.pageYOffset ||
+    document.body.scrollTop ||
+    document.documentElement.scrollTop
+  if (yTopValve > 1) {
+    window.requestAnimationFrame(smoothScrollToTop)
+    scrollTo(0, Math.floor(yTopValve * 0.85))
+  } else {
+    scrollTo(0, 0)
+  }
+}
+setTimeout(() => {
+  document.getElementById('scrollbutton').onclick = smoothScrollToTop
+}, 0)
+// popbutton.js
+const reversePopButton = () => {
+  const scrollButton = document.getElementById('scrollbutton')
+  const menuButton = document.getElementById('menubutton')
+  const reverseButton = document.getElementById('popbutton')
+  const scrollWidth =
+    document.body.scrollWidth || document.documentElement.scrollWidth
+  if (scrollButton.style.opacity === '1') {
+    scrollButton.style.bottom = '32px'
+    scrollButton.style.opacity = '0'
+    reverseButton.style.transform = 'none'
+  } else {
+    reverseButton.style.transform = 'rotate(90deg)'
+    scrollButton.style.bottom = '85px'
+    scrollButton.style.opacity = '1'
+  }
+  const mobileToc = document.getElementById('mobiletoc')
+  if (scrollWidth <= 862 && mobileToc) {
+    if (menuButton.style.opacity === '1') {
+      menuButton.style.right = '32px'
+      menuButton.style.opacity = '0'
+    } else {
+      menuButton.style.right = '85px'
+      menuButton.style.opacity = '1'
+    }
+  }
+  const darkButton = document.querySelector('.darkwidget')
+  const searchButton = document.querySelector('.searchwidget')
+  if (scrollWidth <= 742) {
+    if (darkButton.style.opacity === '1') {
+      darkButton.style.bottom = '32px'
+      darkButton.style.opacity = '0'
+      darkButton.style.transform = 'none'
+    } else {
+      darkButton.style.display = 'flex'
+      reverseButton.style.transform = 'rotate(90deg)'
+      darkButton.style.bottom = '138px'
+      darkButton.style.opacity = '1'
+    }
+
+    if (searchButton.style.opacity === '1') {
+      searchButton.style.bottom = '32px'
+      searchButton.style.opacity = '0'
+      searchButton.style.transform = 'none'
+    } else {
+      searchButton.style.display = 'flex'
+      searchButton.style.transform = 'rotate(90deg)'
+      searchButton.style.bottom = '191px'
+      searchButton.style.opacity = '1'
+    }
+  }
+}
+setTimeout(() => {
+  document
+    .getElementById('popbutton')
+    .addEventListener('click', reversePopButton)
+}, 0)
+// menuButton.js
+function menuClick(event) {
+  const target = event.target
+  const mobileToc = document.getElementById('mobiletoc')
+  if (!mobileToc) {
+    return
+  }
+  if (!mobileToc.contains(target)) {
+    mobileToc.style.display = 'none'
+    mask.remove()
+    document.removeEventListener('click', menuClick)
+  }
+}
+const clickMenuButton = () => {
+  const mobileToc = document.getElementById('mobiletoc')
+  if (!mobileToc) {
+    return
+  }
+  mobileToc.style.display = 'block'
+  const mask = document.createElement('div')
+  mask.id = 'mask'
+  document.body.append(mask)
+  setTimeout(() => {
+    document.addEventListener('click', menuClick)
+  }, 0)
+}
+setTimeout(() => {
+  document.getElementById('menubutton').onclick = clickMenuButton
+}, 0)
+// search.js
+// search button
+function searchClick(event) {
+  const searchContent = document.querySelector('#local-search')
+  if (!searchContent.contains(event.target)) {
+    const searchInput = document.querySelector('#search-input')
+    const content = document.querySelector('#search-content')
+    searchInput.value = ''
+    searchContent.style.display = 'none'
+    content.innerHTML = ''
+    mask.remove()
+    document.removeEventListener('click', searchClick)
+  }
+}
+
+setTimeout(() => {
+  searchControlButton.addEventListener('click', () => {
+    const mask = document.createElement('div')
+    mask.id = 'mask'
+    document.body.append(mask)
+    const searchMain = document.querySelector('#local-search')
+    searchMain.style.display = 'block'
+    setTimeout(() => {
+      document.addEventListener('click', searchClick)
+    }, 0)
+  })
+})
+
+const localSearch = function (path) {
+  fetch(path)
+    .then((res) => res.json())
+    .then((res) => {
+      let input = document.getElementById('search-input')
+      let resultContent = document.getElementById('search-content')
+
+      input.addEventListener('input', function () {
+        let str = '<ul class="search-result-list">'
+        let keyword = this.value.trim().toLowerCase()
+        resultContent.innerHTML = ''
+        if (this.value.trim().length <= 0) {
+          return
+        }
+        res.forEach(function (data) {
+          let isMatch = true
+          if (!data.title || data.title.trim() === '') {
+            data.title = 'Untitled'
+          }
+          const dataTitle = data.title.trim().toLowerCase()
+          const dataContent = data.content
+            .trim()
+            .replace(/<[^>]+>/g, '')
+            .toLowerCase()
+          let firstOccur = -1
+          if (dataContent !== '') {
+            const indexTitle = dataTitle.indexOf(keyword)
+            const indexContent = dataContent.indexOf(keyword)
+            firstOccur = indexContent
+            if (indexTitle < 0 && indexContent < 0) {
+              isMatch = false
+            } else if (indexContent < 0) {
+              firstOccur = 0
             }
-        }
-    });
-
-    if (typeof $.fn.lightGallery === 'function') {
-        $('.article').lightGallery({ selector: '.gallery-item' });
-    }
-    if (typeof $.fn.justifiedGallery === 'function') {
-        if ($('.justified-gallery > p > .gallery-item').length) {
-            $('.justified-gallery > p > .gallery-item').unwrap();
-        }
-        $('.justified-gallery').justifiedGallery();
-    }
-
-    if (!$('.columns .column-right-shadow').children().length) {
-        $('.columns .column-right-shadow').append($('.columns .column-right').children().clone());
-    }
-
-    if (typeof moment === 'function') {
-        $('.article-meta time').each(function() {
-            $(this).text(moment($(this).attr('datetime')).fromNow());
-        });
-    }
-
-    $('.article > .content > table').each(function() {
-        if ($(this).width() > $(this).parent().width()) {
-            $(this).wrap('<div class="table-overflow"></div>');
-        }
-    });
-
-    function adjustNavbar() {
-        const navbarWidth = $('.navbar-main .navbar-start').outerWidth() + $('.navbar-main .navbar-end').outerWidth();
-        if ($(document).outerWidth() < navbarWidth) {
-            $('.navbar-main .navbar-menu').addClass('justify-content-start');
-        } else {
-            $('.navbar-main .navbar-menu').removeClass('justify-content-start');
-        }
-    }
-    adjustNavbar();
-    $(window).resize(adjustNavbar);
-
-    function toggleFold(codeBlock, isFolded) {
-        const $toggle = $(codeBlock).find('.fold i');
-        !isFolded ? $(codeBlock).removeClass('folded') : $(codeBlock).addClass('folded');
-        !isFolded ? $toggle.removeClass('fa-angle-right') : $toggle.removeClass('fa-angle-down');
-        !isFolded ? $toggle.addClass('fa-angle-down') : $toggle.addClass('fa-angle-right');
-    }
-
-    function createFoldButton(fold) {
-        return '<span class="fold">' + (fold === 'unfolded' ? '<i class="fas fa-angle-down"></i>' : '<i class="fas fa-angle-right"></i>') + '</span>';
-    }
-
-    $('figure.highlight table').wrap('<div class="highlight-body">');
-    if (typeof config !== 'undefined'
-        && typeof config.article !== 'undefined'
-        && typeof config.article.highlight !== 'undefined') {
-
-        $('figure.highlight').addClass('hljs');
-        $('figure.highlight .code .line span').each(function() {
-            const classes = $(this).attr('class').split(/\s+/);
-            if (classes.length === 1) {
-                $(this).addClass('hljs-' + classes[0]);
-                $(this).removeClass(classes[0]);
+          } else {
+            isMatch = false
+          }
+          if (isMatch) {
+            str += `<li><a href="${data.url}" class="search-result-title" >'${dataTitle}</a>`
+            const content = data.content
+            if (firstOccur >= 0) {
+              const start = Math.max(0, firstOccur - 12)
+              const end = Math.min(content.length, firstOccur + 12)
+              let matchContent = content.substr(start, end)
+              matchContent = matchContent.replace(
+                new RegExp(keyword, 'gi'),
+                '<em class="search-keyword">' + keyword + '</em>'
+              )
+              str += '<p class="search-result">' + matchContent + '...</p>'
             }
-        });
-
-
-        const clipboard = config.article.highlight.clipboard;
-        const fold = config.article.highlight.fold.trim();
-
-        $('figure.highlight').each(function() {
-            if ($(this).find('figcaption').length) {
-                $(this).find('figcaption').addClass('level is-mobile');
-                $(this).find('figcaption').append('<div class="level-left">');
-                $(this).find('figcaption').append('<div class="level-right">');
-                $(this).find('figcaption div.level-left').append($(this).find('figcaption').find('span'));
-                $(this).find('figcaption div.level-right').append($(this).find('figcaption').find('a'));
-            } else {
-                if (clipboard || fold) {
-                    $(this).prepend('<figcaption class="level is-mobile"><div class="level-left"></div><div class="level-right"></div></figcaption>');
-                }
-            }
-        });
-
-        if (typeof ClipboardJS !== 'undefined' && clipboard) {
-            $('figure.highlight').each(function() {
-                const id = 'code-' + Date.now() + (Math.random() * 1000 | 0);
-                const button = '<a href="javascript:;" class="copy" title="Copy" data-clipboard-target="#' + id + ' .code"><i class="fas fa-copy"></i></a>';
-                $(this).attr('id', id);
-                $(this).find('figcaption div.level-right').append(button);
-            });
-            new ClipboardJS('.highlight .copy'); // eslint-disable-line no-new
+            str += '</li>'
+          }
+        })
+        str += '</ul>'
+        if (str.indexOf('<li>') === -1) {
+          return (resultContent.innerHTML =
+            '<ul><span class="local-search-empty">没有搜索到结果<span></ul>')
         }
-
-        if (fold) {
-            $('figure.highlight').each(function() {
-                if ($(this).find('figcaption').find('span').length > 0) {
-                    const span = $(this).find('figcaption').find('span');
-                    if (span[0].innerText.indexOf('>folded') > -1) {
-                        span[0].innerText = span[0].innerText.replace('>folded', '');
-                        $(this).find('figcaption div.level-left').prepend(createFoldButton('folded'));
-                        toggleFold(this, true);
-                        return;
-                    }
-                }
-                $(this).find('figcaption div.level-left').prepend(createFoldButton(fold));
-                toggleFold(this, fold === 'folded');
-            });
-
-            $('figure.highlight figcaption .fold').click(function() {
-                const $code = $(this).closest('figure.highlight');
-                toggleFold($code.eq(0), !$code.hasClass('folded'));
-            });
-        }
-    }
-
-    const $toc = $('#toc');
-    if ($toc.length > 0) {
-        const $mask = $('<div>');
-        $mask.attr('id', 'toc-mask');
-
-        $('body').append($mask);
-
-        function toggleToc() { // eslint-disable-line no-inner-declarations
-            $toc.toggleClass('is-active');
-            $mask.toggleClass('is-active');
-        }
-
-        $toc.on('click', toggleToc);
-        $mask.on('click', toggleToc);
-        $('.navbar-main .catalogue').on('click', toggleToc);
-    }
-
-    // hexo-util/lib/is_external_link.js
-    function isExternalLink(input, sitehost, exclude) {
-        try {
-            sitehost = new URL(sitehost).hostname;
-        } catch (e) { }
-
-        if (!sitehost) return false;
-
-        // handle relative url
-        let data;
-        try {
-            data = new URL(input, 'http://' + sitehost);
-        } catch (e) { return false; }
-
-        // handle mailto: javascript: vbscript: and so on
-        if (data.origin === 'null') return false;
-
-        const host = data.hostname;
-
-        if (exclude) {
-            exclude = Array.isArray(exclude) ? exclude : [exclude];
-
-            if (exclude && exclude.length) {
-                for (const i of exclude) {
-                    if (host === i) return false;
-                }
-            }
-        }
-
-        if (host !== sitehost) return true;
-
-        return false;
-    }
-
-    if (typeof config !== 'undefined'
-        && typeof config.site.url !== 'undefined'
-        && typeof config.site.external_link !== 'undefined'
-        && config.site.external_link.enable) {
-        $('.article .content a').filter((i, link) => {
-            return link.href
-                && !$(link).attr('href').startsWith('#')
-                && link.classList.length === 0
-                && isExternalLink(link.href,
-                    config.site.url,
-                    config.site.external_link.exclude);
-        }).each((i, link) => {
-            link.relList.add('noopener');
-            link.target = '_blank';
-        });
-    }
-}(jQuery, window.moment, window.ClipboardJS, window.IcarusThemeSettings));
+        resultContent.innerHTML = str
+      })
+    })
+}
+setTimeout(() => {
+  localSearch('/search.json')
+}, 0)
